218dccfe9f5002304fc62b867a3cd2e994858140
nickalaskreynolds <nickalaskreynolds@gmail.com>
2017-11-25 13:19:27 -0600

    fixed with brew audit

---
+++ liberty.rb
@@ -0,0 +1,16 @@
+class Liberty < Formula
+  desc "RDMA core userspace libraries and daemons "
+  homepage "https://github.com/redox-os/binutils-gdb"
+  url 'https://github.com/redox-os/binutils-gdb'
+  head "https://github.com/redox-os/binutils-gdb"
+  version '15'
+
+  def install
+    mkdir "build"
+    Dir.chdir("build")
+    system "../configure", "--disable-debug",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+end

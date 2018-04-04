8a480610ac4cd75b1388907d822f1e3a42429344
nickalaskreynolds <nickalaskreynolds@gmail.com>
2018-04-03 21:44:37 -0500

    update

--- miriad.rb
+++ miriad.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Miriad < Formula
   homepage ''
-  url 'https://www.cfa.harvard.edu/~pwilliam/miriad-macport/miriad-4.3.7.20131227.tar.gz'
-  sha256 'f987c616091b889c850623437886c304dfc058781822f8d5fe943d10d4527401'
+  url 'ftp://ftp.atnf.csiro.au/pub/software/miriad/miriad-linux64.tar.bz2'
+  version '4.3.7.20131227'
   keg_only "This is just the way that MIRIAD works."
 
   depends_on 'gcc'

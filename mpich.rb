3bd61bfc1535e2fddd819364ea633ad524978202
nickalaskreynolds <nickalaskreynolds@gmail.com>
2018-08-24 16:11:53 -0500

    adding mpich

---
+++ mpich.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Mpich < Formula
+  homepage 'http://www.mpich.org/downloads/'
+  url 'http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz'
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+end
